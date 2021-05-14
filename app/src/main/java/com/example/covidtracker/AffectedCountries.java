7bb0b262ea5d4c4cd361e140703c0ef224f9a274
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-05-14 11:37:50 +0530

    first upload

---
+++ app/src/main/java/com/example/covidtracker/AffectedCountries.java
@@ -0,0 +1,29 @@
+package com.example.covidtracker;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.leo.simplearcloader.SimpleArcLoader;
+
+public class AffectedCountries extends AppCompatActivity {
+
+    EditText edtSearch ;
+    ListView listview;
+    SimpleArcLoader simpleArcLoader;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affected_countries);
+
+        edtSearch =findViewById(R.id.edtSearch);
+        listview = findViewById(R.id.listView);
+        simpleArcLoader = findViewById(R.id.loader);
+
+
+    }
+}
